db8131a09f614fce279243a3f3e94e68c1661a45
bomattin <bomattin@butler.edu>
2016-04-03 20:56:22 -0400

    Setup db

--- install.sh
+++ install.sh
@@ -1,10 +1,16 @@
 vagrant init ubuntu/trusty64;
-vagrant ssh;
-curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -;
-sudo apt-get install -y nodejs;
-git clone https://github.com/bomattin/ng-api
-git clone https://github.com/bomattin/ng-client
-mv -r ng-client ng-api/public;
-cd ng-api;
-node bin/www;
+vagrant up;
+vagrant ssh -- -t 'curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -;
+                   sudo apt-get install -y nodejs git postgresql;
+                   sudo su - postgres;
+                   createdb nextgear;
+                   cd /;
+                   git clone https://github.com/bomattin/ng-api;
+                   git clone https://github.com/bomattin/ng-client;
+                   mv -r ng-client ng-api/public;
+                   cd ng-api;
+
+                   node bin/www;
+                   /bin/bash;';
+
 
